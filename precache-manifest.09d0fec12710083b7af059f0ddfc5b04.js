3dfd126b5a350ce2ab6f3820893003a3828acd03
Rafael Diehl <rafaeljuliani1984@gmail.com>
2020-10-14 17:38:05 -0300

    Updates

---
+++ precache-manifest.09d0fec12710083b7af059f0ddfc5b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e697ba71b3a5a0638827cd081857eeb",
+    "url": "/trigonometria/index.html"
+  },
+  {
+    "revision": "56b8bf1c653cdf84d160",
+    "url": "/trigonometria/static/js/2.9c40c2f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trigonometria/static/js/2.9c40c2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1a05425f201902a870",
+    "url": "/trigonometria/static/js/main.f58e4c02.chunk.js"
+  },
+  {
+    "revision": "cc77630ddf410c57c0d6",
+    "url": "/trigonometria/static/js/runtime-main.4e5bafd6.js"
+  },
+  {
+    "revision": "45d383a2dbdb256fdbae2b34f87d0bb2",
+    "url": "/trigonometria/static/media/ciclo-trigonometrico.45d383a2.png"
+  }
+]);
